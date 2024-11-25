706ef80236880aafe029e319b4ef7603ebb62c68
Pawar <pawar1550@gmail.com>
2024-11-25 08:46:06 -0500

    Update Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -447,7 +447,7 @@ survplot(km.by.level)
 
 
 
-table(testing$V2,pred_test) Prediction_test alive dead alive 214 5 dead 31 11 ((214+11)/(nrow(testing)))*100 [1] 86.2069
+table(testing$V2,pred_test) Prediction_test alive dead alive 214 5 dead 31 11 ((214+11)/(nrow(testing)))*100 [1] 86.2069.
 
 
 
